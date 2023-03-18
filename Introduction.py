9d2b7e39c93711dcd2c2b4e58887a1b44e2edc58
Arun-Kakashi <127718562+Arun-Kakashi@users.noreply.github.com>
2023-03-18 13:35:29 +0530

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,171 @@
+##PASSWORD 
+##realUsername = "Arun"
+##realPassword = 123
+##
+##username = input("Enter the username : ")
+##password = int(input("Enter the password : "))
+##
+##if(username == realUsername and password == realPassword):
+##    print('Successfully logged in')
+##    elif(realUsername ==username):
+##        print("change the password")
+##else:
+##    print('Invalid credentials')
+
+
+
+##POSITIVE OR NEGATIVE NUMBER
+##number = int(input("enter the number"))
+##if (number > 0):
+##    print('Number is positive.')
+##else:
+##    print('number is negative')
+
+
+##Calculations
+##/ gives quotient as output with decimals if present
+##// gives quotient as output and removes the decimal even if it is present
+##% gives reminder as output
+##a=23
+##b=34
+##print(a+b)
+##print(a-b)
+##print(a*b)
+##print(a/b)
+##print(a//b)
+##print(a**b)
+##print(a%b)
+
+##SWAPING
+##a=12
+##b=25
+##c=a
+##a=b
+##b=c
+##print(a,b)
+
+##MAX BETWEEN TWO NUMBERS
+##a=10
+##b=20
+##if(a>b):
+##    print("a is greater")
+##else:
+##    print("b is greater")
+
+##MAX BETWEEN THREE NUMBERS
+##a=10
+##b=20
+##c=15
+##if(a>b and a>c):
+##    print("a is greater")
+##elif(b>c):
+##    print("b is greater")
+##else:
+##    print("c is greater")
+
+##CHECK A NUMBER IS POSITIVE, NEGATIVE OR ZERO
+##num=int(input("Enter the number :"))
+##if(num>0):
+##    print("The number is positive")
+##elif(num<0):
+##    print("The number is negative")
+##else:
+##    print("The number is Zero")
+    
+##TO CHECK WHETHER 34A NUMBER IS DIVISIBLE
+##num=int(input("Enter the number"))
+##if(num%5 ==0 and num%11 == 0):
+##    print("The number is divisible by 5 and 11")
+##else:
+##    print("The number is not divisible by 5 and 11")
+        
+##TO CHECK THE NUMBER IS EVEN OR ODD
+##num=int(input("Enter the number"))
+##if(num%2 == 0):
+##    print("The number is even")
+##else:
+##    print("The number is odd")
+
+##TO CHECK LEAP YEAR
+##year=int(input("Enter the year"))
+##if(year%4 ==0):
+##    print("is a leap year")
+##else:
+##    print("is not a leap year")
+
+##TO CHECK THE CHARACTER IS ALPHABET OR NOT
+##character=input("Enter the Character: ")
+##if((character >='a' and character<='z') or (character >='A' and character<='Z')):
+##    print("The character is alphabet")    
+##else:
+##    print("The character is not an alphabet")
+
+##TO CHECK THE ALPHABET IS VOWEL OR CONSONANT
+##al=input("Enter the Character: ")
+##if((al == 'a') or (al=='e') or (al =='i') or (al=='o') or (al=='u') or (al == 'A') or (al=='E') or (al =='I') or (al=='O') or (al=='U')):
+##    print("The alphabet is a vowel")
+##elif((al >='a' and al<='z') or (al >='A' and al<='Z')):
+##     print("The alphabet is a consonant")
+##else:
+##    print("It is not an alphabet")
+
+##TO CHECK THE CHARACTER IS ALPHABET OR DIGIT OR SPECIAL CHARACTER
+##ch=input("Enter the Character: ")
+##if((ch >='a' and ch<='z') or (ch >='A' and ch<='Z')):
+##    print("The character is a alphabet")
+##elif(ch >='0' or ch<='0'):
+##    print("The character is a digit")
+##else:
+##    print("It is a special character")
+
+##TO CHECK THE CHARACTER IS UPPER OR LOWER CASE
+##ch=input("Enter the Alphabet : ")
+##if(ch>='A' and ch<='Z'):
+##    print("The Alphabet is upper case")
+##elif(ch>='a' and ch<='z'):
+##    print("The Alphabet is lower case")
+##else:
+##    print("It is not an alphabet")
+
+##INPUT 5 SUB VALUES AND GET PERCENTAGE AND GRADE
+##A=int(input("Phy mark :"))
+##B=int(input("Chem mark :"))
+##C=int(input("Bio mark :"))
+##D=int(input("Maths mark :"))
+##E=int(input("Comp mark :"))
+##F= ((A+B+C+D+E)/5)
+##if(F >=90):
+##    print("Percentage ",F)
+##    print("Grade A")
+##elif(F >=80):
+##    print("Percentage ",F)
+##    print("Grade B")
+##elif(F >=70):
+##    print("Percentage ",F)
+##    print("Grade C")
+##elif(F >=60):
+##    print("Percentage ",F)
+##    print("Grade D")
+##elif(F >=50):
+##    print("Percentage ",F)
+##    print("Grade E")
+##else:
+##    print("Fail")
+
+##TO INPUT MONTH NUMBER AND PRINT NUMBER OF DAYS IN THAT MONTH
+a=int(input("Enter the month number : ")
+if(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
